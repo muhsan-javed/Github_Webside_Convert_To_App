1020b7fed199f2fd9dacce1ae682cd3226f74045
Muhsan javed <muhsanjaved048@gmail.com>
2021-04-01 10:05:37 +0500

    complete this project

--- app/src/main/java/com/mjtech/muhsantech/MainActivity.java
+++ app/src/main/java/com/mjtech/muhsantech/MainActivity.java
@@ -2,19 +2,29 @@ package com.mjtech.muhsantech;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.AlertDialog;
+import android.app.DownloadManager;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.graphics.Color;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.webkit.CookieManager;
+import android.webkit.DownloadListener;
+import android.webkit.URLUtil;
 import android.webkit.WebChromeClient;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
@@ -23,16 +33,27 @@ import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.Switch;
+import android.widget.Toast;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 
 public class MainActivity extends AppCompatActivity {
 
     WebView webView;
-    private String webUrl = "https://github.com/";
+    private String webUrl = "https://google.com/";
     ProgressBar progressBarWeb;
     ProgressDialog progressDialog;
 
     RelativeLayout relativeLayout;
     Button btnNoInternetConnection;
+
+    SwipeRefreshLayout swipeRefreshLayout;
+
 
 
     @Override
@@ -51,12 +72,91 @@ public class MainActivity extends AppCompatActivity {
 
         btnNoInternetConnection = findViewById(R.id.btnNoConnection);
         relativeLayout = findViewById(R.id.RelativeLayout);
+        swipeRefreshLayout = findViewById(R.id.swipeRefreshLayout);
 
-        checkConnection();
+        swipeRefreshLayout.setColorSchemeColors(Color.BLUE,Color.YELLOW,Color.GREEN);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+               webView.reload();
+            }
+        });
+
+
+        if (savedInstanceState != null){
+            webView.restoreState(savedInstanceState);
+            webView.getSettings().setJavaScriptEnabled(true);
+        }
+        else {
+            // Play Videos Ads
+            webView.getSettings().setJavaScriptEnabled(true);
+            webView.getSettings().setLoadWithOverviewMode(true);
+            webView.getSettings().setUseWideViewPort(true);
+            webView.getSettings().setDomStorageEnabled(true);
+            webView.getSettings().setLoadsImagesAutomatically(true);
+
+            checkConnection();
+        }
+
+        // Play Videos Ads
+//        webView.getSettings().setJavaScriptEnabled(true);
+//        webView.getSettings().setLoadWithOverviewMode(true);
+//        webView.getSettings().setUseWideViewPort(true);
+//        webView.getSettings().setDomStorageEnabled(true);
+//        webView.getSettings().setLoadsImagesAutomatically(true);
+//
+//        checkConnection();
+
+        webView.setDownloadListener(new DownloadListener() {
+            @Override
+            public void onDownloadStart(String url, String userAgent, String contentDisposition, String mimetype, long contentLength) {
+                Dexter.withContext(MainActivity.this)
+                        .withPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                        .withListener(new PermissionListener() {
+                            @Override
+                            public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+
+                                DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
+                                request.setMimeType(mimetype);
+                                String cookies = CookieManager.getInstance().getCookie(url);
+                                request.addRequestHeader("cookie",cookies);
+                                request.addRequestHeader("User-Agent",userAgent);
+                                request.setDescription("Download File....");
+                                request.setTitle(URLUtil.guessFileName(url,contentDisposition,mimetype));
+                                request.allowScanningByMediaScanner();
+                                request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+                                request.setDestinationInExternalPublicDir(
+                                        Environment.DIRECTORY_DOWNLOADS, URLUtil.guessFileName(
+                                                url,contentDisposition,mimetype));
+                                DownloadManager downloadManager = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
+                                downloadManager.enqueue(request);
+                                Toast.makeText(MainActivity.this, "Downloading File...", Toast.LENGTH_SHORT).show();
+
+                            }
+
+                            @Override
+                            public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+
+                            }
+
+                            @Override
+                            public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+                                permissionToken.continuePermissionRequest();
+                            }
+                        }).check();
+            }
+        });
 
 //        webView.loadUrl(webUrl);
 
         webView.setWebViewClient(new WebViewClient(){
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                swipeRefreshLayout.setRefreshing(false);
+                super.onPageFinished(view, url);
+            }
+
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
                view.loadUrl(url);
@@ -173,5 +273,11 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        webView.saveState(outState);
     }
 }
